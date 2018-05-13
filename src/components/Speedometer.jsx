2b5fc0edfff8728577d78eed51511ca4c2606f52
Heikki Mustonen <heikki.mustonen@medisapiens.fi>
2018-05-13 14:58:10 +0300

    Fix last seen for speedometer

--- src/components/Speedometer.jsx
+++ src/components/Speedometer.jsx
@@ -12,16 +12,29 @@ class Speedometer extends React.Component {
         latestTime: 0
     };
     componentWillReceiveProps(newProps) {
-        if (newProps.data && (newProps.data !== this.props.data)) {
+        if (newProps.data && (newProps.selectedCoin !== this.props.selectedCoin)) {
             
             const data = _.find(newProps.data, { 'algo': newProps.selectedCoin.algo });
             const speeds = _.compact(_.map(data.data, '[1].a').map(Number));
-            const time = _.last(data.data)[0]*300
-            console.log('time', time)
+            const last = _.last(data.data);
+            let time = 0;
+            let currentSpeed = 0;
+            if(last[1].a)
+            // If it has speed in latest measurements
+            {
+                currentSpeed = last[1].a*1;
+                time = last[0]*300;
+            } else {
+                // Find last speed & time
+                const lastIndex = _.findLastIndex(data.data, (item) => {return item[1].a});
+                time = data.data[lastIndex][0]*300;
+                currentSpeed = data.data[lastIndex][1].a*1;
+            }
+            
             this.setState({
                 max: _.max(speeds),
                 min: _.min(speeds),
-                current: _.last(speeds),
+                current: currentSpeed,
                 latestTime:  moment.duration(moment.unix(time).diff(moment())).humanize(true)
             })
         }
